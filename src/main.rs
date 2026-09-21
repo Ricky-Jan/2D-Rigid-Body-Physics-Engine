7385ef1ade25a1ffc1a0f649718fc658ff9e6b48
Ricky-Jan <itsrickyjan@gmail.com>
2026-09-21 18:58:14 +0800

    updated scene

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,7 @@ use crate::{
 };
 use macroquad::prelude::*;
 
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "2D Rigid-Body Physics Engine".to_owned(),
-        window_width: 800,       // 適合錄製 GIF 的黃金寬度
-        window_height: 600,      // 適合錄製 GIF 的黃金高度
-        window_resizable: false, // 禁止玩家縮放視窗，確保每次錄影解析度一致
-        ..Default::default()
-    }
-}
-
-#[macroquad::main(window_conf)]
+#[macroquad::main("2D Rigid-Body Physics Engine")]
 async fn main() {
     let mut world = TestScene::create_world();
     let mut camera = Camera::new();
--- src/world.rs
+++ src/world.rs
@@ -34,7 +34,7 @@ impl World {
         let bias = omega / zeta;
         Self {
             context: PhysicsContext {
-                gravity: Vec2::new(0.0, -98.0),
+                gravity: Vec2::new(0.0, -50.0),
                 dt,
                 linear_drag: 0.997,
                 angular_drag: 0.997,
